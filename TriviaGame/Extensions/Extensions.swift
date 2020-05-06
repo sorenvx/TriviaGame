64f48ef4f1934898f4f2f683c0e864812b193ea9
Toni De Gea <sorenvx@hotmail.com>
2020-05-06 12:12:35 +0200

    final app
    Final app

---
+++ TriviaGame/Extensions/Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions.swift
+//  TriviaGame
+//
+//  Created by Toni De Gea on 05/05/2020.
+//  Copyright © 2020 Toni De Gea. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+
+
